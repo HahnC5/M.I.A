47b4dd267ddbc76e108491bd622fe35496ad8d13
HahnC5 <64998747+HahnC5@users.noreply.github.com>
2022-06-24 13:47:13 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,14 @@
+function create() {
+	let circle1 = this.add.circle(50,100,90,0xFFF070);
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 600,
+	scene: {
+		create
+	}
+}
+
+const game = new Phaser.Game(config)
